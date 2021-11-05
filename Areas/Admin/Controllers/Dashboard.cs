7107d04a4b676bea6632ed3f73559d62e0b12b30
hoangnguyen2108 <69739818+hoangnguyen2108@users.noreply.github.com>
2021-11-05 18:31:09 -0400

    Add files via upload

---
+++ Areas/Admin/Controllers/Dashboard.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieDB.Data;
+using MovieDB.Models;
+using MovieDB.Services;
+using MovieDB.Utility;
+using System;
+using System.Threading.Tasks;
+
+namespace MovieDB.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.Admin)]
+    [Area("Admin")]
+    public class Dashboard : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly MovieContext context;
+
+        public Dashboard(ApplicationDbContext db, MovieContext context)
+        {
+            _db = db;
+            this.context = context;
+        }
+
+        public MoviesDB MovieDB { get; set; }
+
+        public async Task<IActionResult> Index(MoviesDB result)
+        {
+            var movies = await context.GetMovies();
+            return View(movies);
+        }
+
+        //create button action
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //saving in db
+        [HttpPost, ActionName("Add")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Added(MovieModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.AddOrUpdate(model);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        //detail view
+        public async Task<IActionResult> Movie(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await context.GetMovie(id);
+            movie.Comments = await context.GetComments(movie.Id);
+            return View(movie);
+        }
+
+        //edit page
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await context.GetMovie(id);
+            return View(movie);
+        }
+
+        //edit and save
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(MovieModel model)
+        {
+            if (model.Id == null)
+            {
+                return NotFound();
+            }
+
+            await context.AddOrUpdate(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await context.RemoveMovie(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Download(string id)
+        {
+            var movie = await context.GetMovieBinary(id);
+            return File(movie.MovieBinary, "video/mp4", movie.Name + ".mp4");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PostComment(string movieId, string comment, int rating, string commentId)
+        {
+            if (!string.IsNullOrWhiteSpace(commentId))
+            {
+                var cmt = await context.GetComment(commentId);
+
+                if (cmt.Date.AddDays(1) < DateTime.Now)
+                {
+                    return BadRequest();
+                }
+            }
+
+            var model = new CommentModel() { Comment = comment, MovieId = movieId, Rating = rating, UserId = User.Identity.Name, Id = commentId };
+            await context.AddOrUpdateComment(model);
+            return Ok();
+        }
+    }
+}
---
+++ Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MovieDB.Data;
+using MovieDB.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDB.Controllers
+{
+    [Area("Users")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly MovieContext context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, MovieContext context)
+        {
+            _logger = logger;
+            _db = db;
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await context.GetDisplayMovies();
+            return View(movies);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string MovieSearch)
+        {
+            ViewData["GetMovies"] = MovieSearch;
+            var movies = await context.GetDisplayMovies();
+            if (!String.IsNullOrEmpty(MovieSearch))
+            {
+                movies = movies.Where(x => x.Name.ToLower().Contains(MovieSearch?.ToLower())).ToList();
+            }
+            return View(movies);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Detail(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await context.GetMovie(id);
+            movie.Comments = await context.GetComments(movie.Id);
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Download(string id)
+        {
+            var movie = await context.GetMovieBinary(id);
+            return File(movie.MovieBinary, "video/mp4", movie.Name + ".mp4");
+        }
+    }
+}
